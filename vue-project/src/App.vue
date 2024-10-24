db8c9a143b7eb7cb39823a887dd456ee4161b8bd
Александр Платонов <100796152+alexandergh1337@users.noreply.github.com>
2024-10-24 23:10:45 +0300

    Add files via upload

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,9 +4,9 @@ import ContactForm from './components/ContactForm.vue';
 import Footer from './components/Footer.vue';
 
 export default {
-  name: "App",  // Исправлено: правильный синтаксис
+  name: "App",
   components: {
-    Navbar,      // Добавлено: компонент Navbar
+    Navbar,
     ContactForm,
     Footer,
   }
@@ -17,6 +17,13 @@ export default {
   <Navbar />
 
   <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="0" class="active" aria-current="true"
+        aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img style="height: 907px;"
@@ -46,6 +53,7 @@ export default {
         </div>
       </div>
     </div>
+
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Предыдущий</span>
--- vue-project/src/components/Footer.vue
+++ vue-project/src/components/Footer.vue
@@ -8,13 +8,13 @@ export default {
     <footer class="bg-light text-center text-lg-start mt-5">
         <div class="container p-4">
             <div class="row">
-                <div class="col-lg-4 col-md-6 mb-4">
+                <div class="col-lg-3 col-md-6 mb-4">
                     <h5 class="text-uppercase">О нас</h5>
-                    <p>ElectroShop — это ваш надежный партнер <br> в области электроники и техники. <br> Мы предлагаем
-                        качественные товары <br> по доступным ценам.</p>
+                    <p>ElectroShop — это ваш надежный партнер в области электроники и техники. Мы предлагаем
+                        качественные товары по доступным ценам.</p>
                 </div>
 
-                <div class="col-lg-4 col-md-6 mb-4">
+                <div class="col-lg-3 col-md-6 mb-4">
                     <h5 class="text-uppercase">Контакты</h5>
                     <ul class="list-unstyled">
                         <li>
@@ -35,7 +35,7 @@ export default {
                     </ul>
                 </div>
 
-                <div class="col-lg-4 col-md-12 mb-4">
+                <div class="col-lg-3 col-md-12 mb-4">
                     <h5 class="text-uppercase">Навигация</h5>
                     <ul class="list-unstyled mb-0">
                         <li>
@@ -53,24 +53,24 @@ export default {
                     </ul>
                 </div>
 
-                <div class="col-lg-12 mb-4">
-                    <h5 class="text-uppercase">Мы в соцсетях</h5>
-                    <ul class="list-unstyled mb-0 d-flex justify-content-center align-items-center">
-                        <li class="mx-2">
+                <div class="col-lg-3 col-md-12 mb-4">
+                    <h5 class="txt-soc text-uppercase">Мы в соцсетях</h5>
+                    <ul class="list-unstyled d-flex">
+                        <li class="me-3">
                             <a href="https://t.me" class="text-dark" aria-label="Telegram">
                                 <img style="width: 48px; height: 48px;"
                                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/512px-Telegram_2019_Logo.svg.png"
                                     alt="Telegram">
                             </a>
                         </li>
-                        <li class="mx-2">
+                        <li class="me-3">
                             <a href="https://vk.com" class="text-dark" aria-label="ВКонтакте">
                                 <img style="width: 48px; height: 48px;"
                                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/archive/2/21/20220814010126%21VK.com-logo.svg/120px-VK.com-logo.svg.png"
                                     alt="ВКонтакте">
                             </a>
                         </li>
-                        <li class="mx-2">
+                        <li>
                             <a href="https://zen.yandex.ru" class="text-dark" aria-label="Яндекс Дзен">
                                 <img style="width: 48px; height: 48px;"
                                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Yandex_Zen_logo_icon.svg/512px-Yandex_Zen_logo_icon.svg.png"
@@ -104,7 +104,39 @@ a:hover {
     text-decoration: underline;
 }
 
-ul li span {
-    text-decoration: none;
+.txt-soc {
+    text-align: center;
+}
+
+ul.list-unstyled {
+    margin: 0;
+    padding: 0;
+}
+
+ul.list-unstyled li {
+    list-style: none;
+}
+
+ul.list-unstyled.d-flex li {
+    display: inline-block;
+}
+
+ul.list-unstyled.d-flex li.me-3 {
+    margin-right: 20px;
+}
+
+ul.list-unstyled li img {
+    width: 48px;
+    height: 48px;
+}
+
+ul.d-flex {
+    justify-content: center;
+}
+
+@media (min-width: 768px) {
+    ul.d-flex {
+        justify-content: center;
+    }
 }
 </style>
